38ddf0c15207e9d6eed2099b096c2969b657c1e5
Przemek Grochowski <sovvly@protonmail.com>
2019-03-14 22:28:09 +0100

    Created new test and page objects for site

---
+++ tivixDemo/tivixDemo/PageObjects/AboutPage.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace tivixDemo.PageObjects
+{
+    class AboutPage
+    {
+        private IWebDriver driver;
+
+        [FindsBy(How = How.XPath, Using = "//div/button[@class = 'tivix - button']")]
+        private IWebElement joinOutTeamBtn;
+
+        [FindsBy(How = How.XPath, Using = "//div/p[text()='YEAR FOUNDED']")]
+        private IWebElement yearFoundedLbl;
+
+        [FindsBy(How = How.XPath, Using = "//div/p[text()='TALENTED EMPLOYEES']")]
+        private IWebElement talentedLbl;
+
+        [FindsBy(How = How.XPath, Using = "//div/p[text()='OFFICES WORLDWIDE']")]
+        private IWebElement officesLbl;
+
+        public AboutPage(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        public void validAboutPage()
+        {
+            Assert.True(joinOutTeamBtn.Displayed, "Missing join our team button");
+            Assert.True(yearFoundedLbl.Displayed, "Missing year founded label");
+            Assert.True(talentedLbl.Displayed, "Missing talented emploees label");
+            Assert.True(officesLbl.Displayed, "Missing offices worldwide label");
+        }
+    }
+
+
+}
---
+++ tivixDemo/tivixDemo/PageObjects/MainPage.cs
@@ -0,0 +1,43 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace tivixDemo.PageObjects
+{
+    class MainPage
+    {
+        private IWebDriver driver;
+
+        [FindsBy(How = How.XPath, Using = "//a/li/span[text()='services']")]
+        private IWebElement servicesBtn;
+
+        [FindsBy(How = How.XPath, Using = "//a/li/span[text()='case studies']")]
+        private IWebElement caseStudiesBn;
+
+        [FindsBy(How = How.XPath, Using = "//a/li/span[text()='about']")]
+        private IWebElement aboutBtn;
+
+        [FindsBy(How = How.XPath, Using = "//a/li/span[text()='blog']")]
+        private IWebElement blogBtn;
+
+        [FindsBy(How = How.XPath, Using = "//a/li/span[text()='resources']")]
+        private IWebElement resourcesBtn;
+
+        [FindsBy(How = How.XPath, Using = "//a/li/span[text()='carriers']")]
+        private IWebElement carriersBtn;
+
+        [FindsBy(How = How.XPath, Using = "//a/li/span[text()='contact']")]
+        private IWebElement contactBtn;
+
+        public MainPage(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        public AboutPage goToAboutPage()
+        {
+            aboutBtn.Click();
+            return new AboutPage(driver);
+        }
+    }
+}
