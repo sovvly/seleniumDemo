2b6a3a07de4cc2f544a8fb01d2d0801b036753b1
Przemek Grochowski <sovvly@protonmail.com>
2019-03-14 22:51:19 +0100

    added gecko driver and renamed test

---
+++ tivixDemo/tivixDemo/Tests/AboutT/validAboutPageAfterEntrenceFromMainPage.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using tivixDemo.PageObjects;
+
+namespace tivixDemo.Tests.AboutT
+{
+    class validAboutPageAfterEntrenceFromMainPage
+    {
+        private IWebDriver driver;
+
+        [SetUp]
+         public void startBrowser()
+        {  
+            FirefoxDriverService service = FirefoxDriverService.CreateDefaultService(@"C:\Users\yoursUserName\tivixDemo\tivixDemo\tivixDemo\", "geckodriver.exe");   
+            service.FirefoxBinaryPath = @"C:\Program Files\Mozilla Firefox\firefox.exe";
+            driver = new FirefoxDriver(service);
+            driver.Navigate().GoToUrl("https://www.tivix.com");
+        }
+
+        [Test]
+        public void validAboutPageAfterEntrenceFromMainPageTest()
+        {        
+
+            var mainPage = new MainPage(driver);
+
+            AboutPage aboutPage = mainPage.goToAboutPage();
+            aboutPage.validAboutPage();
+            driver.Close();
+        }
+    }
+}
